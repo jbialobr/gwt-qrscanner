32aa10448a6781173bc6e9b4bc910b48fb48deff
Janusz Białobrzewski <jbialobr@o2.pl>
2016-07-03 09:36:35 +0200

    QRScanner based on the zxing project

---
+++ core/src/main/java/com/google/zxing/web/CanvasLuminanceSource.java
@@ -0,0 +1,228 @@
+/*
+ * Copyright 2009 ZXing authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.zxing.web;
+
+import com.google.gwt.canvas.client.Canvas;
+import com.google.gwt.canvas.dom.client.CanvasPixelArray;
+import com.google.gwt.canvas.dom.client.Context2d;
+import com.google.gwt.canvas.dom.client.ImageData;
+import com.google.zxing.LuminanceSource;
+
+/**
+ * This LuminanceSource implementation is meant for html canvas.
+ *
+ * @author dswitkin@google.com (Daniel Switkin)
+ * @author Sean Owen
+ * @author code@elektrowolle.de (Wolfgang Jung)
+ * gwt implementation: Janusz Białobrzewski
+ */
+public final class CanvasLuminanceSource extends LuminanceSource
+{
+
+    /**
+     * Number of colors at each location in the array.
+     * 
+     * Because image data is stored as RGBA, this is 4.
+     */
+    private static final int NUM_COLORS = 4;
+
+    /**
+     * Offsets for each color use RGBA ordering.
+     */
+    private static final int OFFSET_RED = 0;
+    private static final int OFFSET_GREEN = 1;
+    private static final int OFFSET_BLUE = 2;
+    private static final int OFFSET_ALPHA = 3;
+
+    private final Canvas image;
+    private final ImageData imagedata;
+    private final int left;
+    private final int top;
+
+    public CanvasLuminanceSource(Canvas image)
+    {
+        this(image, 0, 0, image.getCoordinateSpaceWidth(), image.getCoordinateSpaceHeight());
+    }
+
+    public CanvasLuminanceSource(Canvas aImage, int left, int top, int width, int height)
+    {
+        super(width, height);
+
+        int sourceWidth = aImage.getCoordinateSpaceWidth();
+        int sourceHeight = aImage.getCoordinateSpaceHeight();
+        if(left + width > sourceWidth || top + height > sourceHeight)
+        {
+            throw new IllegalArgumentException("Crop rectangle does not fit within image data.");
+        }
+
+        this.image = Canvas.createIfSupported();
+        this.image.setPixelSize(sourceWidth, sourceHeight);
+        this.image.setCoordinateSpaceHeight(sourceHeight);
+        this.image.setCoordinateSpaceWidth(sourceWidth);
+
+        imagedata = aImage.getContext2d().getImageData(left, top, width, height);
+        CanvasPixelArray data = imagedata.getData();
+
+        for (int i = 0; i < data.getLength(); i += NUM_COLORS)
+        {
+            if((data.get(i + 3) & 0xFF) == 0)
+            {
+                // The color of fully-transparent pixels is irrelevant. They are
+                // often, technically, fully-transparent
+                // black (0 alpha, and then 0 RGB). They are often used, of
+                // course as the "white" area in a
+                // barcode image. Force any such pixel to be white:
+                int avg = 0xFF;
+                data.set(i + OFFSET_RED, avg);
+                data.set(i + OFFSET_GREEN, avg);
+                data.set(i + OFFSET_BLUE, avg);
+                data.set(i + OFFSET_ALPHA, avg);
+            }
+            else
+            {
+                int avg = (data.get(i + OFFSET_RED) + data.get(i + OFFSET_GREEN) + data.get(i
+                    + OFFSET_BLUE)) / 3;
+                data.set(i + OFFSET_RED, avg);
+                data.set(i + OFFSET_GREEN, avg);
+                data.set(i + OFFSET_BLUE, avg);
+            }
+        }
+
+        this.image.getContext2d().putImageData(imagedata, left, top);
+        this.left = left;
+        this.top = top;
+    }
+
+    @Override
+    public byte[] getRow(int y, byte[] row)
+    {
+        if(y < 0 || y >= getHeight())
+        {
+            throw new IllegalArgumentException("Requested row is outside the image: " + y);
+        }
+        int width = getWidth();
+        if(row == null || row.length < width)
+        {
+            row = new byte[width];
+        }
+
+        int yoffset = top + y;
+        for (int i = 0; i < width; i++)
+        {
+            row[i] = (byte) imagedata.getRedAt(left + i, yoffset);
+        }
+
+        return row;
+    }
+
+    @Override
+    public byte[] getMatrix()
+    {
+        int width = getWidth();
+        int height = getHeight();
+        int area = width * height;
+        byte[] matrix = new byte[area];
+        for (int i = 0; i < width; i++)
+            for (int j = 0; j < height; j++)
+            {
+                int pixelInt = imagedata.getRedAt(left + i, top + j);
+                byte pixelByte = (byte) pixelInt;
+                matrix[i + j * width] = pixelByte;
+            }
+        return matrix;
+    }
+
+    @Override
+    public boolean isCropSupported()
+    {
+        return true;
+    }
+
+    @Override
+    public LuminanceSource crop(int left, int top, int width, int height)
+    {
+        return new CanvasLuminanceSource(image, this.left + left, this.top + top, width, height);
+    }
+
+    /**
+     * This is always true, since the image is a gray-scale image.
+     *
+     * @return true
+     */
+    @Override
+    public boolean isRotateSupported()
+    {
+        return true;
+    }
+
+    @Override
+    public LuminanceSource rotateCounterClockwise()
+    {
+        int sourceWidth = image.getCoordinateSpaceWidth();
+        int sourceHeight = image.getCoordinateSpaceHeight();
+
+        // Note width/height are flipped since we are rotating 90 degrees.
+        Canvas rotatedImage = Canvas.createIfSupported();
+        rotatedImage.setCoordinateSpaceWidth(sourceWidth);
+        rotatedImage.setCoordinateSpaceHeight(sourceHeight);
+        
+        Context2d ctx = rotatedImage.getContext2d();
+        // Rotate 90 degrees counterclockwise.
+        ctx.transform(0.0, -1.0, 1.0, 0.0, 0.0, sourceWidth);
+        ctx.putImageData(imagedata, sourceWidth, sourceHeight);
+        // Maintain the cropped region, but rotate it too.
+        int width = getWidth();
+        return new CanvasLuminanceSource(rotatedImage, top, sourceWidth - (left + width),
+            getHeight(), width);
+    }
+
+    @Override
+    public LuminanceSource rotateCounterClockwise45()
+    {
+        int sourceWidth = image.getCoordinateSpaceWidth();
+        int sourceHeight = image.getCoordinateSpaceHeight();
+
+        int width = getWidth();
+        int height = getHeight();
+
+        int oldCenterX = left + width / 2;
+        int oldCenterY = top + height / 2;
+
+        Canvas rotatedImage = Canvas.createIfSupported();
+        int sourceDimension = Math.max(sourceWidth, sourceHeight);
+        rotatedImage.setCoordinateSpaceWidth(sourceDimension);
+        rotatedImage.setCoordinateSpaceHeight(sourceDimension);
+        
+        Context2d ctx = rotatedImage.getContext2d();
+        ctx.translate(oldCenterX, oldCenterY);
+        // Rotate 45 degrees counterclockwise.
+        ctx.rotate(-45 * Math.PI / 180);
+        ctx.translate(-oldCenterX, -oldCenterY);
+        ctx.putImageData(imagedata, sourceWidth, sourceHeight);
+
+
+        int halfDimension = Math.max(width, height) / 2;
+        int newLeft = Math.max(0, oldCenterX - halfDimension);
+        int newTop = Math.max(0, oldCenterY - halfDimension);
+        int newRight = Math.min(sourceDimension - 1, oldCenterX + halfDimension);
+        int newBottom = Math.min(sourceDimension - 1, oldCenterY + halfDimension);
+
+        return new CanvasLuminanceSource(rotatedImage, newLeft, newTop, newRight - newLeft,
+            newBottom - newTop);
+    }
+
+}
---
+++ core/src/main/java/com/google/zxing/web/ScannerWidget.java
@@ -0,0 +1,207 @@
+package com.google.zxing.web;
+
+import com.google.gwt.canvas.client.Canvas;
+import com.google.gwt.dom.client.Element;
+import com.google.gwt.media.client.Video;
+import com.google.gwt.user.client.Timer;
+import com.google.gwt.user.client.rpc.AsyncCallback;
+import com.google.gwt.user.client.ui.FlowPanel;
+import com.google.zxing.Binarizer;
+import com.google.zxing.BinaryBitmap;
+import com.google.zxing.Result;
+import com.google.zxing.common.HybridBinarizer;
+import com.google.zxing.qrcode.QRCodeReader;
+
+public class ScannerWidget extends FlowPanel
+{
+
+    private Video video = Video.createIfSupported();
+    private Canvas canvas = Canvas.createIfSupported();
+    private QRCodeReader qrReader = new QRCodeReader();
+    private double lastScanTime;
+    private int scanInterval = 300;
+    private AsyncCallback<Result> callback;
+    private Timer scanTimer;
+    private int snapImageMaxSize = 300;
+
+    public ScannerWidget(AsyncCallback<Result> callback)
+    {
+        this.callback = callback;
+        createScanTimer();
+        add(video);
+        video.setStyleName("qrPreviewVideo");
+        video.setAutoplay(true);
+        setWebcam(video.getElement(), this);
+    }
+
+    private void createScanTimer()
+    {
+        scanTimer = new Timer()
+        {
+
+            @Override
+            public void run()
+            {
+                scan();
+            }
+        };
+
+    }
+
+    private BinaryBitmap createSnapImage()
+    {
+        int w, h;
+        w = video.getVideoWidth();
+        h = video.getVideoHeight();
+        if(w > 0 && h > 0)
+        {
+            if(w > h)
+            {
+                if(snapImageMaxSize < w)
+                {
+                    h = h * snapImageMaxSize / w;
+                    w = snapImageMaxSize;
+                }
+            }
+            else
+            {
+                if(snapImageMaxSize < h)
+                {
+                    w = w * snapImageMaxSize / h;
+                    h = snapImageMaxSize;
+                }                
+            }
+            canvas.setCoordinateSpaceWidth(w);
+            canvas.setCoordinateSpaceHeight(h);
+            canvas.getContext2d().drawImage(video.getVideoElement(), 0, 0, w, h);
+
+            CanvasLuminanceSource lsource = new CanvasLuminanceSource(canvas);
+            Binarizer binarizer = new HybridBinarizer(lsource);
+            BinaryBitmap snapImage = new BinaryBitmap(binarizer);
+            return snapImage;
+        }
+        return null;
+    }
+
+    private void startScanning()
+    {
+        scanTimer.schedule(scanInterval);
+    }
+
+    private void videoAttached()
+    {
+        startScanning();
+    }
+
+    private void scan()
+    {
+        try
+        {
+            BinaryBitmap bitmap = createSnapImage();
+            if(bitmap != null)
+            {
+                qrReader.reset();
+                Result result = qrReader.decode(bitmap);
+                onSuccess(result);
+            }
+        }
+        catch(Exception e)
+        {
+            onError(e);
+        }
+        finally
+        {
+            startScanning();
+        }
+    }
+
+    private void onSuccess(Result result)
+    {
+        callback.onSuccess(result);
+    }
+
+    private void onError(Exception e)
+    {
+        callback.onFailure(e);
+    }
+
+    public native void setWebcam(Element videoElement, ScannerWidget scanner) /*-{
+
+		function success(stream) {
+
+			var v = videoElement;
+			v.src = $wnd.URL.createObjectURL(stream);
+			scanner.@com.google.zxing.web.ScannerWidget::videoAttached()();
+		}
+
+		function error(error) {
+			return;
+		}
+
+		var n = $wnd.navigator;
+
+		if (n.mediaDevices && n.mediaDevices.getUserMedia) {
+			n.mediaDevices.getUserMedia({
+				video : {
+					facingMode : "environment"
+				},
+				audio : false
+			}).then(success);
+		} else {
+			MediaStreamTrack.getSources(function(sourceInfos) {
+				var videoSource = null;
+
+				for (var i = 0; i != sourceInfos.length; ++i) {
+					var sourceInfo = sourceInfos[i];
+					if (sourceInfo.kind === 'video'
+							&& sourceInfo.facing === 'environment') {
+
+						videoSource = sourceInfo.id;
+					}
+				}
+
+				sourceSelected(videoSource);
+			});
+
+			function sourceSelected(videoSource) {
+				var constraints = {
+					audio : false,
+					video : {
+						optional : [ {
+							sourceId : videoSource
+						} ]
+					}
+				};
+
+				if (n.getUserMedia) {
+					n.getUserMedia(constraints, success, error);
+				} else if (n.webkitGetUserMedia) {
+					n.webkitGetUserMedia(constraints, success, error);
+				} else if (n.mozGetUserMedia) {
+					n.mozGetUserMedia(constraints, success, error);
+				}
+			}
+		}
+    }-*/;
+
+    public int getScanInterval()
+    {
+        return scanInterval;
+    }
+
+    public void setScanInterval(int scanInterval)
+    {
+        this.scanInterval = scanInterval;
+    }
+
+    public int getSnapImageMaxSize()
+    {
+        return snapImageMaxSize;
+    }
+
+    public void setSnapImageMaxSize(int snapImageMaxSize)
+    {
+        this.snapImageMaxSize = snapImageMaxSize;
+    }
+
+}
